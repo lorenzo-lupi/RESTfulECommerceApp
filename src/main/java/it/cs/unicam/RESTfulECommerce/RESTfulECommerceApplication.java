cdb181e12779b5743bbc108bd6eed1f3dc030fb6
lorenzo <lupi.lorenzo001@gmail.com>
2023-11-28 17:25:02 +0100

    Project creation

---
+++ src/main/java/it/cs/unicam/RESTfulECommerce/RESTfulECommerceApplication.java
@@ -0,0 +1,13 @@
+package it.cs.unicam.RESTfulECommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RESTfulECommerceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RESTfulECommerceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/it/cs/unicam/RESTfulECommerce/ResTfulECommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package it.cs.unicam.RESTfulECommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResTfulECommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
